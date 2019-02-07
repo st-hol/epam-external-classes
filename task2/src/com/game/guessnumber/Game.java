3ce4f6417550e5c62c4dc1dae01cdefe05587305
Stas <stanislav.holovachuk@gmail.com>
2019-02-07 17:04:35 +0200

    Initial commit

--- task2/src/com/game/guessnumber/Game.java
+++ task2/src/com/game/guessnumber/Game.java
@@ -1,16 +1,20 @@
 package com.game.guessnumber;
 
+import java.util.ArrayList;
+
 public class Game {
     // Text's constants
     public static final String INPUT_INT_DATA = "Input INT value = ";
     public static final String WRONG_INPUT_INT_DATA = "Wrong input! Repeat please! ";
     public static final String WINNER = "CONGRATULATIONS! YOU HAVE WON!";
 
+
     private Player player;
     private Opponent opponent;
     private StatusChecker checker;
+    private ArrayList<Integer> previousAttempts = new ArrayList<>();
 
-    public Game(Player player,Opponent opponent){
+    public Game(Player player, Opponent opponent){
         this.player = player;
         this.opponent = opponent;
         this.checker = new StatusChecker();
@@ -21,25 +25,41 @@ public class Game {
     }
 
 
+    public void showStat(){
+        System.out.printf("You made %d attempts \nLog: ", previousAttempts.size());
+        for(Integer item : previousAttempts){
+            System.out.print(" " + item);
+        }
+    }
+
     public void playGame(){
+
         opponent.setRightNumber(Opponent.rand());
 
         //displays right answer. used for test
         //System.out.println( + opponent.getRightNumber());
 
         while (true) {
-            System.out.printf("Guess a number in range [ %d and %d ]  ", checker.getLowerBound(), checker.getUpperBound());
+            System.out.printf("\nGuess a number in range [ %d and %d ] \n ", checker.getLowerBound(), checker.getUpperBound());
+          //  System.out.println("Previous attempts:" + );
+
             int guessedNumber = player.guessNumber();
+            previousAttempts.add(guessedNumber);
 
             if (guessedNumber <= checker.getLowerBound() || guessedNumber >= checker.getUpperBound()){
+                System.out.println("Out of range...");
+                showStat();
                 continue;
             }
             if (guessedNumber > opponent.getRightNumber()) {
                 checker.setUpperBound(guessedNumber);
+                showStat();
             } else if (guessedNumber < opponent.getRightNumber()) {
                 checker.setLowerBound(guessedNumber);
+                showStat();
             } else {
                 System.out.println(WINNER);
+                showStat();
                 break;
             }
         }
